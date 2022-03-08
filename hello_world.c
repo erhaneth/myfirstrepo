326c2f599caf2c4f2a8d576633f425f114b41c38
Zeid Tisnes <ztisnes@Gielinor.localdomain>
2022-03-08 15:38:41 -0800

    this is now an actual C file that outputs the old message

--- hello_world.c
+++ hello_world.c
@@ -1,2 +1,7 @@
-Zeid is my teacher right now and I'm learning with him about Git and GitHub (Obviously this is not C, but an example in general)
+#include <stdio.h>
 
+int		main(void)
+{
+	printf("Zeid is my teacher right now and I'm learning with him about Git and GitHub (Obviously this is not C, but an example in general)");
+	return (0);
+}
